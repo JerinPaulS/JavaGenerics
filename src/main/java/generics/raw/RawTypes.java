8533e8e38f0deec80e8387957d512df26a11f10a
JerinPaulS <jerinsprograms@gmail.com>
2024-05-14 10:29:45 +0530

    Generics basics

---
+++ src/main/java/generics/raw/RawTypes.java
@@ -0,0 +1,25 @@
+package generics.raw;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RawTypes {
+
+    public static void main(String[] args) {
+        List strings = new ArrayList<>();
+        strings.add("This");
+        strings.add("is");
+        strings.add("a");
+        strings.add("RAW");
+        strings.add("list");
+        strings.add("of");
+        strings.add("strings");
+
+        for (Object o : strings) {
+            String s = (String) o;
+            System.out.printf("%7s has %d characters%n", s, s.length());
+        }
+    }
+
+}
